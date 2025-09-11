37f751d135469ca5de978506c7391ea986c1a9a5
enyasystem <enyaelvis@gmail.com>
2025-09-11 12:05:08 +0100

    feat: Implement CRUD operations for Post model in views

--- day09-django-blog/blog/views.py
+++ day09-django-blog/blog/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from .models import Post
+import json
 
-# Create your views here.
+# Create post
+@csrf_exempt
+def create_post(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        post = Post.objects.create(title=data['title'],
+                                   content = data['content']
+                                   )
+        return JsonResponse({'id': post.id, 'title': post.title, 'content': post.content})
+
+#READ all posts
+def list_posts(request):
+    posts = Post.objects.all().values()
+    return JsonResponse(list(posts), safe=False)
+
+#UPDATE a post
+@csrf_exempt
+def update_post(request, post_id):
+    if request.method == 'PUT':
+        data = json.loads(request.body)
+        try:
+            post = Post.objects.get(id=post_id)
+            post.title = data.get('title', post.title)
+            post.content = data.get('content', post.content)
+            post.save()
+            return JsonResponse({'message': 'Post updated successfully'})
+        except Post.DoesNotExist:
+            return JsonResponse({'error': 'Post not found'}, status=404)
+        
+#DELETE a post
+@csrf_exempt # to exempt this view from CSRF verification for simplicity
+def delete_post(request, post_id):
+    if request.method == 'DELETE':
+        try:
+            post = Post.objects.get(id=post_id)
+            post.delete()
+            return JsonResponse({'message': 'Post deleted successfully'})
+        except Post.DoesNotExist:
+            return JsonResponse({'error': 'Post not found'}, status=404)
