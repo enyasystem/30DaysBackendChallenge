ae4025cf5c0766f5e77dd65c6b9670a4b7171967
enyasystem <enyaelvis@gmail.com>
2025-09-11 10:22:11 +0100

    feat: Define Post model with title, content, and created_at fields

--- day09-django-blog/blog/models.py
+++ day09-django-blog/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200) # blog post title
+    content = models.TextField()              # blog post content
+    created_at = models.DateTimeField(auto_now_add=True) # timestamp when created
+
+    def __str__(self): # string representation of the model
+        return self.title # return the title of the post as its string representation
